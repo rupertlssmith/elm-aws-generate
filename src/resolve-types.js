916494c4bd84dd101d421af23a5f5299461f12b7
Rupert Smith <rupert@thesett.com>
2019-08-07 13:24:23 +0100

    Cleaned up some commented out code.

--- src/resolve-types.js
+++ src/resolve-types.js
@@ -87,16 +87,6 @@ module.exports = (shapesWithoutNames, { inputShapes, outputShapes }) => {
             : `${lowCam(value.type)}ToString`;
     const queryEncoder = base => `AWS.Core.Encode.addDictToQueryArgs (${queryEncoderType}) "${base}"`;
 
-    // const jsonEncoder = isEnumOfFloats(key)
-    //   ? 'AWS.Core.Enum.toFloat >> Result.withDefault 0.0 >> String.fromFloat'
-    //   : (value.type == 'Int')
-    //     ? `${jsonEncode}.dict identity ${jsonEncode}.int`
-    //     : (value.type == 'Float')
-    //       ? `${jsonEncode}.dict identity ${jsonEncode}.float`
-    //       : (value.type == 'String')
-    //         ? `${jsonEncode}.dict identity ${jsonEncode}.string`
-    //         : `${lowCam(value.type)}Encoder`;
-
     const jsonEncoder = isEnumOfFloats(key)
       ? 'AWS.Core.Enum.toFloat >> Result.withDefault 0.0 >> String.fromFloat'
       : `${jsonEncode}.dict identity (${value.jsonEncoder})`
